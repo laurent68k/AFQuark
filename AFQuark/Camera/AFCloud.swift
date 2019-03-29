1982ecf8d9cbfee3415b5beadce43efbe0a2af15
Laurent Favard <laurent68k@gmail.com>
2019-03-29 19:05:15 +0100

    New class

---
+++ AFQuark/Camera/AFCloud.swift
@@ -0,0 +1,93 @@
+//
+//  ICloud
+//  My external Pod project
+//
+//  Created by Laurent Favard on 26/03/2019
+//  Copyright © 2019 Laurent Favard. All rights reserved.
+//
+
+import UIKit
+
+open class AFiCloud {
+    
+    //--------------------------------------------------------------------------------------------------------------
+    
+    static let shared = AFiCloud()
+    
+    //--------------------------------------------------------------------------------------------------------------
+    /**
+     Cloud folder to use inside the shared cloud folder Application
+     */
+    static var cloudFolder : String {
+        
+        return UIDevice.current.name
+    }
+    
+    /**
+     File Manager to use
+     */
+    private var fileManager: FileManager {
+        
+        return FileManager.default
+    }
+    
+    /**
+     URL to access the working folder for the iCloud iOS daemon
+     */
+    private var iCloudDocumentsURL : URL? 
+    
+    //--------------------------------------------------------------------------------------------------------------
+    
+    private init() {
+        
+        //  Start an asynchronous request to get the iCloud url to use:
+        //  Apple: this method might take a nontrivial amount of time to set up iCloud and return the requested URL,
+        //  you should always call it from a secondary thread
+        DispatchQueue.main.async {
+            
+            //  Keyword 'Documents' is very IMPORTANT and must be added at the hand
+            let remoteUrl = self.fileManager.url(forUbiquityContainerIdentifier: nil)?.appendingPathComponent("Documents")
+            
+            self.iCloudDocumentsURL = remoteUrl
+        }
+    }
+    
+    //--------------------------------------------------------------------------------------------------------------
+    
+    open func synchronize(fromSource localCloudFolder: URL) {
+        
+        //is iCloud working?
+        if let iCloudDocumentsURL = self.iCloudDocumentsURL {
+            
+            let iCouldDestination = iCloudDocumentsURL.appendingPathComponent( AFiCloud.cloudFolder )
+            
+            //Create the Directory if it doesn't exist
+            if (!self.fileManager.fileExists(atPath: iCloudDocumentsURL.path, isDirectory: nil)) {
+                
+                do {
+                    try self.fileManager.createDirectory(at: iCloudDocumentsURL, withIntermediateDirectories: true, attributes:nil)
+                }
+                catch {
+                    print(error.localizedDescription)
+                }
+            }
+            
+            do {
+                try self.fileManager.removeItem(at: iCouldDestination)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+            
+            do {
+                try self.fileManager.copyItem(at: localCloudFolder, to: iCouldDestination)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        else {
+            print("iCloud is NOT working or not available yet")
+        }
+    }
+}
---
+++ AFQuark/Camera/AFPdf.swift
@@ -0,0 +1,65 @@
+//
+//  PdfGen.swift
+//  My external Pod project
+//
+//  Created by Laurent Favard on 26/03/2019
+//  Copyright © 2019 Laurent Favard. All rights reserved.
+//
+
+import UIKit
+
+class AFPdf {
+
+    //--------------------------------------------------------------------------------------------------------------
+    private static var temporaryDirectory : URL? {
+        
+        if #available(iOS 10.0, *) {
+            
+            return FileManager.default.temporaryDirectory
+        }
+        
+        return try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor:nil, create:false)
+    }
+    //--------------------------------------------------------------------------------------------------------------
+
+    /**
+        Return a URL to the PDF document which contains the visit desscription
+     */
+    class func create(named fileName:String, from formatter: UIViewPrintFormatter) -> URL? {
+        
+        let render = UIPrintPageRenderer()
+        render.addPrintFormatter(formatter, startingAtPageAt: 0)
+        
+        //  Create Paper Size for print
+        let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8)
+        let printable = page.insetBy(dx: 0, dy: 0)
+        
+        render.setValue(NSValue(cgRect: page), forKey: "paperRect")
+        render.setValue(NSValue(cgRect: printable), forKey: "printableRect")
+        
+        // 4. Create PDF context and draw        
+        let filedata = NSMutableData()
+        UIGraphicsBeginPDFContextToData(filedata, CGRect.zero, nil)
+        
+        for i in 1...render.numberOfPages {
+            
+            UIGraphicsBeginPDFPage();
+            let bounds = UIGraphicsGetPDFContextBounds()
+            render.drawPage(at: i - 1, in: bounds)
+        }
+        
+        UIGraphicsEndPDFContext();
+        
+        // 5. Save PDF file you can also save this file by using another button
+        
+        var documentsDirectory = self.temporaryDirectory
+        documentsDirectory?.appendPathComponent( fileName )
+        
+        if let documentsDirectory = documentsDirectory {
+            
+            filedata.write(to: documentsDirectory, atomically: true)
+        }
+        
+        return documentsDirectory
+    }    
+}
